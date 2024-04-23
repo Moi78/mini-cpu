d54cd1317a15345d1253b3f303ccfb406c6c564e
mathis <mathis.contact@gmail.com>
2024-04-23 11:47:54 +0200

    memory sprite pos bare bones

--- gfx_controller.vhd
+++ gfx_controller.vhd
@@ -25,7 +25,8 @@ architecture a_gc of gfx_controller is
 	type bg_state is (
 		idle_bg, 
 		fetch_bg_rq, fetch_bg_get, fetch_bg_end,
-		fetch_sprite_flags, fetch_sf_get, fetch_sf_end, 
+		fetch_sprite_flags, fetch_sf_get, fetch_sf_end,
+		fetch_spos_start, fetch_spos_xlow, fetch_spos_xhigh, fetch_spos_ylow, fetch_spos_yhigh, fetch_spos_end,
 		fetch_bg_free_vram
 	);
 	type disp_state is (bg_color, sprite_line);
@@ -40,7 +41,9 @@ architecture a_gc of gfx_controller is
 	signal i_pxx			  : unsigned(15 downto 0);
 	
 	signal i_sprx          : unsigned(15 downto 0);
+	signal i_spry			  : unsigned(15 downto 0);
 	signal i_spr_cpt       : integer range 0 to 32;
+	signal i_spr_ylimit    : unsigned(15 downto 0);
 	
 	signal i_sprite_flags  : std_logic_vector(7 downto 0);
 	signal i_line_drawn_spr: std_logic_vector(7 downto 0);
@@ -86,7 +89,14 @@ begin
 				
 				when fetch_sprite_flags => i_bg_state <= fetch_sf_get;
 				when fetch_sf_get => i_bg_state <= fetch_sf_end;
-				when fetch_sf_end => i_bg_state <= fetch_bg_free_vram;
+				when fetch_sf_end => i_bg_state <= fetch_spos_start;
+				
+				when fetch_spos_start => i_bg_state <= fetch_spos_xlow;
+				when fetch_spos_xlow => i_bg_state <= fetch_spos_xhigh;
+				when fetch_spos_xhigh => i_bg_state <= fetch_spos_ylow;
+				when fetch_spos_ylow => i_bg_state <= fetch_spos_yhigh;
+				when fetch_spos_yhigh => i_bg_state <= fetch_spos_end;
+				when fetch_spos_end => i_bg_state <= fetch_bg_free_vram;
 				
 				when fetch_bg_free_vram => i_bg_state <= idle_bg;
 				when idle_bg => i_bg_state <= idle_bg;
@@ -116,7 +126,7 @@ begin
 			i_sprite_drawn(0) <= '1';
 		end if;
 		
-		if (i_pxx > i_sprx) and (i_line_drawn_spr(0) = '0') and (i_pxy < 32) then
+		if (i_pxx > i_sprx) and (i_line_drawn_spr(0) = '0') and (i_pxy > i_spry) and (i_pxy < i_spr_ylimit) then
 			i_disp_state <= sprite_line;
 			i_line_drawn_spr(0) <= '1';
 			
@@ -150,6 +160,9 @@ begin
 	px_color <= i_px_color;
 	i_pxy <= unsigned(pxy);
 	i_pxx <= unsigned(pxx);
-	i_sprx <= to_unsigned(200, 16);
+	
+	i_sprx <= to_unsigned(500, 16);
+	i_spry <= to_unsigned(200, 16);
+	i_spr_ylimit <= i_spry + 32;
 	
 end architecture a_gc;
